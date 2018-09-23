3d9de87f9b0c7757ce18f6e2f3730aeb4a687a38
Sebass van Boxel <svboxel@gmail.com>
2018-09-23 19:58:04 +0200

    Syntax improvements

--- index.js
+++ index.js
@@ -1,50 +1,66 @@
-/**
- * This is the entry point for your Probot App.
- * @param {import('probot').Application} app - Probot's Application class.
- */
+const APPROVED = 'APPROVED';
+const SUCCESS = 'success';
+
 module.exports = app => {
-  // Your code here
   app.log('Yay, the app was loaded!')
 
   app.on('status', async context => {
-    const state = context.payload.state
-    const statusDescription = context.payload.description
-    const owner = context.payload.repository.owner.login
-    const repo = context.payload.repository.name
-    const ref = context.payload.sha
+    const { 
+      github: {
+        pullRequests,
+        repos,
+        search,
+      },
+      payload: { 
+        description, 
+        repository, 
+        sha, 
+        state, 
+      }, 
+    } = context;
 
-    if (state !== 'success') {
-      context.log.info(`${statusDescription} state is "${state}", not merging.`)
+    const owner = repository.owner.login
+    const repo = repository.name
+
+    if (state !== SUCCESS) {
+      context.log.info(`${description} state is "${state}", not merging.`)
       return
     }
 
-    const { data: { state: combinedState } } = await context.github.repos.getCombinedStatusForRef({owner, repo, ref})
+    const { 
+      data: { 
+        state: combinedStatusState
+       } 
+      } = await repos.getCombinedStatusForRef({owner, repo, ref: sha})
 
-    if (combinedState !== 'success') {
-      context.log.info(`Combined status for ${statusDescription} is "${combinedState}", not merging.`)
+    if (combinedStatusState !== SUCCESS) {
+      context.log.info(`Combined status for ${description} is "${combinedState}", not merging.`)
       return
     }
 
-    const result = await context.github.search.issues({q: `${ref}+type:pr`})
+    const result = await search.issues({q: `${sha}+type:pr`})
     const number = result.data.items[0].number
 
-    const pr = await context.github.pullRequests.get({owner, repo, number})
+    const pr = await pullRequests.get({owner, repo, number})
 
-    const reviews = await context.github.pullRequests.getReviews({ owner, repo, number, per_page: 100 })
+    const reviews = await pullRequests.getReviews({ owner, repo, number, per_page: 100 })
 
     if (reviews.length !== 0) {
-      context.log.info(`PR #${number} isn't reviewed yet, not merging`)
+      context.log.info(`PR #${number} isn't reviewed yet, not merging.`)
       return
     }
 
-    if (!reviews.every(review => review.state === 'APPROVED')) {
-      context.log.info(`PR #${number} isn't approved by all reviewers yet. Not merging`)
+    if (!reviews.every(review => review.state === APPROVED)) {
+      context.log.info(`PR #${number} isn't approved by all reviewers yet. Not merging.`)
       return
     }
 
     if (pr.data.mergeable) {
       context.log.info(`Merging PR #${number}`)
-      await context.github.pullRequests.merge({owner, repo, number, sha: ref})
+      await pullRequests.merge({owner, repo, number, sha})
+      return
     }
+
+    return
   })
 }
