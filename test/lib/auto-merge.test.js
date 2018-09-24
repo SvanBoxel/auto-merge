3983e49fa8786b125cf1b35ed16892083df07b79
Sebass van Boxel <svboxel@gmail.com>
2018-09-24 21:47:50 +0200

    App specs part 2

--- test/lib/auto-merge.test.js
+++ test/lib/auto-merge.test.js
@@ -1,19 +1,21 @@
-const autoMerge = require('../../lib/auto-merge');
+const autoMerge = require('../../lib/auto-merge')
 const findPRWithRef = require('../../lib/utils/find-pr-with-ref')
 const statusEventPayload = require('../fixtures/status.json')
-const searchIssuesResponse = require('../fixtures/search.json')
 const getPullRequestResponse = require('../fixtures/pull_request.json')
+const getReviewsResponse = require('../fixtures/get_reviews.json')
 
 jest.mock('../../lib/utils/find-pr-with-ref', () => jest.fn())
 
 describe('autoMerge function', () => {
   let context
-  let getPullRequest
   let getCombinedStatus
-  
+  let getPullRequest
+  let getReviews
+
   beforeEach(() => {
-    getCombinedStatus = jest.fn().mockReturnValue(Promise.resolve(searchIssuesResponse))
+    getCombinedStatus = jest.fn().mockReturnValue(Promise.resolve({ data: {} }))
     getPullRequest = jest.fn().mockReturnValue(Promise.resolve(getPullRequestResponse))
+    getReviews = jest.fn().mockReturnValue(Promise.resolve(getReviewsResponse))
     context = {
       log: {
         info: jest.fn(),
@@ -21,45 +23,148 @@ describe('autoMerge function', () => {
       },
       payload: JSON.parse(JSON.stringify(statusEventPayload)),
       github: {
-        pullRequests: {
-          get: getPullRequest
-        },
         repos: {
           getCombinedStatusForRef: getCombinedStatus
+        },
+        pullRequests: {
+          get: getPullRequest,
+          getReviews,
+          merge: jest.fn()
         }
       }
     }
   })
-  
+
   describe('status state is NOT "success"', () => {
     beforeEach(() => {
-      context.payload.state = 'pending';
+      context.payload.state = 'pending'
       autoMerge(context)
-    });
-    
+    })
+
     it('should log cannot merge the branch', () => {
-      expect(context.log.info).toBeCalledWith(`\"${context.payload.description}\" state is \"${context.payload.state}\", not merging.`)
+      expect(context.log.info).toBeCalledWith(`"${context.payload.description}" state is "${context.payload.state}", not merging.`)
     })
   })
-  
+
   describe('status state is "success"', () => {
     beforeEach(() => {
-      context.payload.state = 'success';
+      context.payload.state = 'success'
       autoMerge(context)
-    });
-    
+    })
+
     describe('getCombinedStatusForRef method does NOT return "success"', () => {
-      let state;
+      let state
 
       beforeEach(async () => {
-        state = pending
-        getCombinedStatus.mockReturnValue(Promise.resolve({ data: { state } } ))
+        state = 'pending'
+        getCombinedStatus.mockReturnValue(Promise.resolve({ data: { state } }))
         await autoMerge(context)
-      });
-      
-      it('should log it didn\'t delete the branch', () => {
-        expect(context.log.info).toBeCalledWith(`Combined status for \"${context.payload.description}\" is \"${state}\", not merging.`)
       })
-    });
+
+      it('should log cannot merge the branch', () => {
+        expect(context.log.info).toBeCalledWith(`Combined status for "${context.payload.description}" is "${state}", not merging.`)
+      })
+    })
+
+    describe('getCombinedStatusForRef method does return "success"', () => {
+      let state
+
+      beforeEach(async () => {
+        state = 'success'
+        getCombinedStatus.mockReturnValue(Promise.resolve({ data: { state } }))
+      })
+
+      describe('findPRWithRef method does return null', () => {
+        beforeEach(async () => {
+          findPRWithRef.mockReturnValue(null)
+          await autoMerge(context)
+        })
+
+        it('should log no PRs are related to this branch', () => {
+          expect(context.log.info).toBeCalledWith(`No PR related to commit "${context.payload.sha}" found.`)
+        })
+      })
+
+      describe('findPRWithRef method returns a PR', () => {
+        beforeEach(() => {
+          findPRWithRef.mockReturnValue(getPullRequestResponse.data)
+        })
+
+        describe('PR does NOT have any reviewers assigned', () => {
+          beforeEach(async () => {
+            context.github.pullRequests.getReviews.mockReturnValue(Promise.resolve([]))
+            await autoMerge(context)
+          })
+
+          it('should log no reviewers are assigned', () => {
+            expect(context.log.info).toBeCalledWith(`PR #${getPullRequestResponse.data.number} isn't reviewed yet, not merging.`)
+          })
+        })
+
+        describe('PR is NOT approved by all reviewers', () => {
+          beforeEach(async () => {
+            const reviews = [
+              getReviewsResponse[0],
+              {
+                ...getReviewsResponse[1],
+                state: 'PENDING'
+              }
+            ]
+
+            context.github.pullRequests.getReviews.mockReturnValue(reviews)
+            await autoMerge(context)
+          })
+
+          it('should log PR isn\'t approved by all reviewers', () => {
+            expect(context.log.info).toBeCalledWith(`PR #${getPullRequestResponse.data.number} isn't approved by all reviewers yet. Not merging.`)
+          })
+        })
+
+        describe('PR is approved by all reviewers', () => {
+          beforeEach(() => {
+            context.github.pullRequests.getReviews.mockReturnValue(getReviewsResponse)
+          })
+
+          describe('PR is NOT mergeable', () => {
+            beforeEach(async () => {
+              findPRWithRef.mockReturnValue({
+                ...getPullRequestResponse.data,
+                mergeable: false
+              })
+
+              await autoMerge(context)
+            })
+
+            it('should log that the PR isn\'t mergeable', () => {
+              expect(context.log.info).toBeCalledWith(`PR #${getPullRequestResponse.data.number} isn't mergeable`)
+            })
+          })
+
+          describe('PR is mergeable', () => {
+            beforeEach(async () => {
+              findPRWithRef.mockReturnValue({
+                ...getPullRequestResponse.data,
+                mergeable: true
+              })
+
+              await autoMerge(context)
+            })
+
+            it('should call the merge method', () => {
+              expect(context.github.pullRequests.merge).toBeCalledWith({
+                owner: context.payload.repository.owner.login,
+                repo: context.payload.repository.name,
+                number: getPullRequestResponse.data.number,
+                sha: context.payload.sha
+              })
+            })
+
+            it('should log the merge method', () => {
+              expect(context.log.info).toBeCalledWith(`Merged PR #${getPullRequestResponse.data.number}`)
+            })
+          })
+        })
+      })
+    })
   })
 })
