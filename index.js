122f03967824d3d47bc0d03b745d8336ea032682
Sebass van Boxel <svboxel@gmail.com>
2018-09-29 17:44:48 +0200

    Reverted index.js

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const autoMerge = require('./lib/auto-merge')
-const withConfig = require('./lib/utils/with-config')
 
 module.exports = app => {
   app.log('Loaded auto-merge GitHub Application')
-  app.on(['pull_request_review.submitted', 'status'], (context) => {
-    const config = withConfig(context)(({ auto_merge_on = ['status', 'review']} ) => auto_merge_on)
-    console.log(config);
-    autoMerge(context);
-  })
+  app.on('status', autoMerge)
 }
